e8cfc23f262e1e23c2eac5774952c936c1f5d22e
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2024-05-24 22:24:37 -0500

    React refactor

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Graph from './components/Graph';
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/graph" element={<Graph />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
